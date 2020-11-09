4af24754367cbd315196ac8da3063f12c31107e6
mbarkdull <mb2337@cornell.edu>
2020-11-09 11:05:23 -0500

    Adding code to make bar plots and generating those plots for proportion of Black graduates.

--- IPEDSDataProcessing.R
+++ IPEDSDataProcessing.R
@@ -1,15 +1,18 @@
 # Load packages:
 library(plyr)
 library(tidyverse)
+library(scales)
+library(ggpubr)
+theme_set(theme_pubr())
 
-# Read data files:
+# Read completion data files:
 IPEDS2017 <- read_csv("./IPEDSData/CSV_1192020-936.csv")
 IPEDS2016 <- read_csv("./IPEDSData/CSV_1192020-687.csv")
 IPEDS2019 <- read_csv("./IPEDSData/CSV_1192020-519.csv")
 IPEDS2018 <- read_csv("./IPEDSData/CSV_1192020-366.csv")
 IPEDS2015 <- read_csv("./IPEDSData/CSV_1192020-194.csv")
 
-# Process the data:
+# Process the completion data:
 IPEDS2015 <- select(IPEDS2015, -c("unitid", "year", "C2015_A.First or Second Major", "C2015_A.CIP Code -  2010 Classification", "C2015_A.Award Level code", "CipTitle", "IDX_C"))
 IPEDS2015$ProportionBlack <- IPEDS2015$"C2015_A_RV.Black or African American total" / IPEDS2015$"C2015_A_RV.Grand total"
 IPEDS2015Long <- pivot_longer(IPEDS2015, cols = "C2015_A_RV.Grand total":"ProportionBlack")
@@ -30,4 +33,19 @@ IPEDS2019 <- select(IPEDS2019, -c("unitid", "year", "C2019_A.First or Second Maj
 IPEDS2019$ProportionBlack <- IPEDS2019$"C2019_A.Black or African American total" / IPEDS2019$"C2019_A.Grand total"
 IPEDS2019Long <- pivot_longer(IPEDS2019, cols = "C2019_A.Grand total":"ProportionBlack")
 
+# Plot completion data:
+BlackProportionPlotting <- function(data, plotname) {
+  plotname <- ggplot(data = data) + geom_col(mapping = aes(x = reorder(`institution name`, -ProportionBlack), y = ProportionBlack)) + theme(axis.text.x = element_text(angle = 90), axis.text = element_text(size = 2), axis.title = element_text(size = 3)) + annotate("segment", x = "Cornell University", xend = "Cornell University", y = 1, yend = 0, size = 1, alpha = 0.2, arrow = arrow())
+  plot(plotname)
+  #ggsave(filename = filename, plot = last_plot(), device = "pdf")
+}
 
+BPC2015 <- BlackProportionPlotting(IPEDS2015, "BPC2015")
+BPC2016 <- BlackProportionPlotting(IPEDS2016, "BPC2016")
+BPC2017 <- BlackProportionPlotting(IPEDS2017, "BPC2017")
+BPC2018 <- BlackProportionPlotting(IPEDS2018, "BPC2018")
+BPC2019 <- BlackProportionPlotting(IPEDS2019, "BPC2019")
+
+BPCCombined <- ggarrange(BPC2015, BPC2016, BPC2017, BPC2018, BPC2019, ncol = 2, nrow = 3)
+plot(BPCCombined)
+ggsave(filename = "BPCCombined.pdf", plot = last_plot(), device = "pdf")
