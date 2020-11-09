2486cac78c968150c890b8c09327ecdb98e24ea0
mbarkdull <mb2337@cornell.edu>
2020-11-09 11:26:42 -0500

    Rotating plots ninety degrees to make institution labels more readable.

--- IPEDSDataProcessing.R
+++ IPEDSDataProcessing.R
@@ -34,17 +34,34 @@ IPEDS2019$ProportionBlack <- IPEDS2019$"C2019_A.Black or African American total"
 IPEDS2019Long <- pivot_longer(IPEDS2019, cols = "C2019_A.Grand total":"ProportionBlack")
 
 # Plot completion data:
-BlackProportionPlotting <- function(data, plotname) {
-  plotname <- ggplot(data = data) + geom_col(mapping = aes(x = reorder(`institution name`, -ProportionBlack), y = ProportionBlack)) + theme(axis.text.x = element_text(angle = 90), axis.text = element_text(size = 2), axis.title = element_text(size = 3)) + annotate("segment", x = "Cornell University", xend = "Cornell University", y = 1, yend = 0, size = 0.1,  arrow = arrow())
+BlackProportionPlotting <- function(data, plotname, title) {
+  plotname <- ggplot(data = data) + 
+    geom_col(mapping = aes(x = reorder(`institution name`, -ProportionBlack), y = ProportionBlack)) + 
+    theme(axis.text.x = element_text(angle = 90), 
+          axis.text = element_text(size = 2), 
+          axis.title = element_text(size = 2), 
+          axis.line = element_line(size = 0.1), 
+          axis.ticks = element_line(size = 0.05), 
+          plot.title = element_text(size = 5)) + 
+    annotate("segment", 
+             x = "Cornell University", 
+             xend = "Cornell University", 
+             y = 1, 
+             yend = 0, 
+             size = 0.1,  
+             arrow = arrow()) + 
+    labs(title = title, 
+         x = "Institution", 
+         y = "Proportion of PhD Awardees who are Black") +
+    coord_flip()
   plot(plotname)
-  #ggsave(filename = filename, plot = last_plot(), device = "pdf")
 }
 
-BPC2015 <- BlackProportionPlotting(IPEDS2015, "BPC2015")
-BPC2016 <- BlackProportionPlotting(IPEDS2016, "BPC2016")
-BPC2017 <- BlackProportionPlotting(IPEDS2017, "BPC2017")
-BPC2018 <- BlackProportionPlotting(IPEDS2018, "BPC2018")
-BPC2019 <- BlackProportionPlotting(IPEDS2019, "BPC2019")
+BPC2015 <- BlackProportionPlotting(IPEDS2015, "BPC2015", "Proportion of \nBlack PhD Recipients (2015)")
+BPC2016 <- BlackProportionPlotting(IPEDS2016, "BPC2016", "Proportion of \nBlack PhD Recipients (2016)")
+BPC2017 <- BlackProportionPlotting(IPEDS2017, "BPC2017", "Proportion of \nBlack PhD Recipients (2017)")
+BPC2018 <- BlackProportionPlotting(IPEDS2018, "BPC2018", "Proportion of \nBlack PhD Recipients (2018)")
+BPC2019 <- BlackProportionPlotting(IPEDS2019, "BPC2019", "Proportion of \nBlack PhD Recipients (2019)")
 
 BPCCombined <- ggarrange(BPC2015, BPC2016, BPC2017, BPC2018, BPC2019, ncol = 2, nrow = 3)
 plot(BPCCombined)
