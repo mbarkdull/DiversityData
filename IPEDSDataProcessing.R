c18081d249f377ccb1db17ddb830d51dcd8e0575
mbarkdull <mb2337@cornell.edu>
2020-11-09 10:07:36 -0500

    Adding IPEDS data on race/ethnicity for EEB PhD grads, and script to process data.

---
+++ IPEDSDataProcessing.R
@@ -0,0 +1,33 @@
+# Load packages:
+library(plyr)
+library(tidyverse)
+
+# Read data files:
+IPEDS2017 <- read_csv("./IPEDSData/CSV_1192020-936.csv")
+IPEDS2016 <- read_csv("./IPEDSData/CSV_1192020-687.csv")
+IPEDS2019 <- read_csv("./IPEDSData/CSV_1192020-519.csv")
+IPEDS2018 <- read_csv("./IPEDSData/CSV_1192020-366.csv")
+IPEDS2015 <- read_csv("./IPEDSData/CSV_1192020-194.csv")
+
+# Process the data:
+IPEDS2015 <- select(IPEDS2015, -c("unitid", "year", "C2015_A.First or Second Major", "C2015_A.CIP Code -  2010 Classification", "C2015_A.Award Level code", "CipTitle", "IDX_C"))
+IPEDS2015$ProportionBlack <- IPEDS2015$"C2015_A_RV.Black or African American total" / IPEDS2015$"C2015_A_RV.Grand total"
+IPEDS2015Long <- pivot_longer(IPEDS2015, cols = "C2015_A_RV.Grand total":"ProportionBlack")
+
+IPEDS2016 <- select(IPEDS2016, -c("unitid", "year", "C2016_A.First or Second Major", "C2016_A.CIP Code -  2010 Classification", "C2016_A.Award Level code", "CipTitle", "IDX_C"))
+IPEDS2016$ProportionBlack <- IPEDS2016$"C2016_A_RV.Black or African American total" / IPEDS2016$"C2016_A_RV.Grand total"
+IPEDS2016Long <- pivot_longer(IPEDS2016, cols = "C2016_A_RV.Grand total":"ProportionBlack")
+
+IPEDS2017 <- select(IPEDS2017, -c("unitid", "year", "C2017_A.First or Second Major", "C2017_A.CIP Code -  2010 Classification", "C2017_A.Award Level code", "CipTitle", "IDX_C"))
+IPEDS2017$ProportionBlack <- IPEDS2017$"C2017_A_RV.Black or African American total" / IPEDS2017$"C2017_A_RV.Grand total"
+IPEDS2017Long <- pivot_longer(IPEDS2017, cols = "C2017_A_RV.Grand total":"ProportionBlack")
+
+IPEDS2018 <- select(IPEDS2018, -c("unitid", "year", "C2018_A.First or Second Major", "C2018_A.CIP Code -  2010 Classification", "C2018_A.Award Level code", "CipTitle", "IDX_C"))
+IPEDS2018$ProportionBlack <- IPEDS2018$"C2018_A_RV.Black or African American total" / IPEDS2018$"C2018_A_RV.Grand total"
+IPEDS2018Long <- pivot_longer(IPEDS2018, cols = "C2018_A_RV.Grand total":"ProportionBlack")
+
+IPEDS2019 <- select(IPEDS2019, -c("unitid", "year", "C2019_A.First or Second Major", "C2019_A.CIP Code -  2010 Classification", "C2019_A.Award Level code", "CipTitle", "IDX_C"))
+IPEDS2019$ProportionBlack <- IPEDS2019$"C2019_A.Black or African American total" / IPEDS2019$"C2019_A.Grand total"
+IPEDS2019Long <- pivot_longer(IPEDS2019, cols = "C2019_A.Grand total":"ProportionBlack")
+
+
